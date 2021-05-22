8c70bfdf3852b3d0aa1ecce64d6bd59edb8362b3
Ben Ubois <ben@benubois.com>
2021-05-22 13:24:16 -0700

    Update

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -5,13 +5,25 @@ timeout 30
 preload_app true
 user "app", "app"
 
-working_directory "/srv/apps/privacy-please/current"
+base_directory = File.expand_path("..", __dir__)
+shared_directory = File.join(File.expand_path("..", base_directory), "shared")
+shared_directory = File.directory?(shared_directory) ? shared_directory : base_directory
 
-listen "/run/privacy-please.sock"
-pid "/tmp/privacy-please.pid"
+puts '-----------------------------'
+puts base_directory
+puts shared_directory
+puts '-----------------------------'
+
+working_directory base_directory
+
+listen "#{shared_directory}/tmp/unicorn.sock"
+pid "#{shared_directory}/tmp/unicorn.pid"
+
+stderr_path "#{shared_directory}/log/unicorn.stderr.log"
+stdout_path "#{shared_directory}/log/unicorn.stdout.log"
 
 before_fork do |server, worker|
-  old_pid = "/run/unicorn.pid.oldbin"
+  old_pid = "#{shared_directory}/tmp/unicorn.pid.oldbin"
   if File.exist?(old_pid) && server.pid != old_pid
     begin
       Process.kill("QUIT", File.read(old_pid).to_i)
