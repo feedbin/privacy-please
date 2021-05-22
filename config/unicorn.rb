7ea6b13c2905ccd1ffb14a5c6bfbee6e98b50518
Ben Ubois <ben@benubois.com>
2021-05-22 09:42:03 -0700

    Unicorn config.

---
+++ config/unicorn.rb
@@ -0,0 +1,18 @@
+require "etc"
+
+worker_processes Etc.nprocessors
+timeout 30
+preload_app true
+user "app", "app"
+
+working_directory "/srv/apps/privacy-please/current"
+
+listen "/run/privacy-please.sock"
+pid "/run/privacy-please.pid"
+
+before_fork do |server, worker|
+  old_pid = "/run/unicorn.pid.oldbin"
+  if File.exist?(old_pid) && server.pid != old_pid
+    Process.kill("QUIT", File.read(old_pid).to_i) rescue Errno::ENOENT, Errno::ESRCH
+  end
+end
