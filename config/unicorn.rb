7656b91ab3668c153c6a5ae257db9ffe4fec5c00
Ben Ubois <ben@benubois.com>
2022-12-13 20:47:01 +0100

    Switch back to USR2 so new code gets picked up.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -9,3 +9,14 @@ pid               File.join(shared_directory, "tmp", "unicorn.pid")
 listen            File.join(shared_directory, "tmp", "unicorn.sock")
 logger            Logger.new($stdout)
 worker_processes  ENV.fetch("WEB_CONCURRENCY", Etc.nprocessors)
+
+before_fork do |server, worker|
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+end
