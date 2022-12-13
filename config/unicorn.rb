d4303935e125c47889aae178a17bf97e92e90f41
Ben Ubois <ben@benubois.com>
2022-12-13 19:33:32 +0100

    Remove redundant listen.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,13 +1,10 @@
 require "etc"
 
-working_directory  File.expand_path("..", __dir__)
 shared_directory = File.join(File.expand_path("..", ENV["PWD"]), "shared")
 shared_directory = File.directory?(shared_directory) ? shared_directory : ENV["PWD"]
 
-pid    File.join(shared_directory, "tmp", "unicorn.pid")
-listen File.join(shared_directory, "tmp", "unicorn.sock")
-
-logger Logger.new($stdout)
-
-worker_processes ENV.fetch("WEB_CONCURRENCY", Etc.nprocessors)
-listen           ENV.fetch("PORT", 3000)
+pid               File.join(shared_directory, "tmp", "unicorn.pid")
+listen            File.join(shared_directory, "tmp", "unicorn.sock")
+logger            Logger.new($stdout)
+worker_processes  ENV.fetch("WEB_CONCURRENCY", Etc.nprocessors)
+working_directory File.expand_path("..", __dir__)
