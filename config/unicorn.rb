4025655c6cf95cd19b8c56953885a0153abf1343
Ben Ubois <ben@benubois.com>
2022-12-13 19:39:02 +0100

    Fix order.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,7 @@
 require "etc"
 
+working_directory File.expand_path("..", __dir__)
+
 shared_directory = File.join(File.expand_path("..", ENV["PWD"]), "shared")
 shared_directory = File.directory?(shared_directory) ? shared_directory : ENV["PWD"]
 
@@ -7,4 +9,3 @@ pid               File.join(shared_directory, "tmp", "unicorn.pid")
 listen            File.join(shared_directory, "tmp", "unicorn.sock")
 logger            Logger.new($stdout)
 worker_processes  ENV.fetch("WEB_CONCURRENCY", Etc.nprocessors)
-working_directory File.expand_path("..", __dir__)
