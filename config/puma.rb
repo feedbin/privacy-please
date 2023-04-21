b68f80c5bf03d3b29a72996f9eb07d4d07bf755d
Ben Ubois <ben@benubois.com>
2023-04-21 17:24:38 +0200

    Puma

---
+++ config/puma.rb
@@ -0,0 +1,13 @@
+require 'etc'
+
+workers ENV.fetch("WEB_CONCURRENCY", Etc.nprocessors)
+threads_count = ENV.fetch("MAX_THREADS", 24)
+threads threads_count, threads_count
+
+environment ENV.fetch("RACK_ENV", "development")
+
+shared_directory = File.join(File.expand_path("..", ENV["PWD"]), "shared")
+shared_directory = File.directory?(shared_directory) ? shared_directory : ENV["PWD"]
+
+pidfile File.join(shared_directory, "tmp", "puma.pid")
+bind    File.join("unix://", shared_directory, "tmp", "puma.sock")
