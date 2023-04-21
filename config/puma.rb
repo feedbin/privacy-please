cfc13bea9e8348f94daff33543583e1cce699697
Ben Ubois <ben@benubois.com>
2023-04-21 17:30:13 +0200

    Config check

--- config/puma.rb
+++ config/puma.rb
@@ -6,6 +6,8 @@ threads threads_count, threads_count
 
 environment ENV.fetch("RACK_ENV", "development")
 
+puts ENV.inspect
+
 shared_directory = File.join(File.expand_path("..", ENV["PWD"]), "shared")
 shared_directory = File.directory?(shared_directory) ? shared_directory : ENV["PWD"]
 
